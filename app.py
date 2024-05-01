8256f90bafce15ac137089fc1caded0969f4d0ef
krystian.rozwag <krystek_1@onet.eu>
2024-05-01 16:45:49 +0200

    methods for main app loop like reading post, blog and tests for them

--- app.py
+++ app.py
@@ -1,11 +1,54 @@
 blogs = dict()
-
+from blog import Blog
 MENU_PROMPT = 'Enter "c" to create a new blog, "l" to list blogs, "r" to read one, "p" to create a post or "q" to quit: '
+
+
+
+
 def menu():
     print_blogs()
     selection = input(MENU_PROMPT)
-
+    while selection != 'q':
+        if selection == 'c':
+            ask_create_blog()
+        elif selection == 'l':
+            print_blogs()
+        elif selection == 'r':
+            ask_read_blog()
+        elif selection == 'p':
+            ask_create_post()
+        selection = input(MENU_PROMPT)
 
 def print_blogs():
     for key, blog in blogs.items():
         print(f'{key} - {blog}')
+
+def ask_create_blog():
+    title = input(f'Enter your blog title: ')
+    author = input(f'Enter your blog author: ')
+    blogs[title] = Blog(title, author)
+
+def ask_read_blog():
+    title = input(f'Enter your blog title: ')
+
+    blog = blogs[title]
+    if blog is not None:
+        print_posts(blog)
+    else:
+        print('Blog does not exist')
+
+def print_posts(blog):
+    for post in blog.posts:
+        print_post(post)
+def print_post(post):
+    print(f"Title: {post.title}, content: {post.content}")
+def ask_create_post():
+    blog_name = input(f'Enter your blog title: ')
+
+    blog = blogs[blog_name]
+    if blog is not None:
+        title = input(f'Enter your post title: ')
+        content = input(f'Enter your post content: ')
+        blog.create_post(title, content)
+    else:
+        print('Blog does not exist')
--- tests/system/test_app.py
+++ tests/system/test_app.py
@@ -2,6 +2,9 @@ from unittest import TestCase
 from unittest.mock import patch
 import app
 from blog import Blog
+from post import Post
+
+
 class TestApp(TestCase):
 
 
@@ -20,3 +23,33 @@ class TestApp(TestCase):
         with patch('builtins.print') as mocked_print:
             app.print_blogs()
             mocked_print.assert_called_with('Test - Test by Test Author (0 posts)')
+
+    def test_ask_create_blog(self):
+        with patch('builtins.input') as mocked_input:
+            mocked_input.side_effect = ('Test', 'Test Author')
+            app.ask_create_blog()
+
+            self.assertIsNotNone(app.blogs.get('Test'))
+    def test_ask_read_blog(self):
+        blog = Blog('Test', 'Test Author')
+        app.blogs = {'Test': blog}
+        with patch('builtins.input', return_value='Test') as mocked_input:
+            with patch('app.print_posts') as mocked_print_posts:
+                app.ask_read_blog()
+
+                mocked_print_posts.assert_called_with(blog)
+
+    def test_print_posts(self):
+        blog = Blog('Test', 'Test Author')
+        blog.create_post('Test Post', 'Test Content')
+        app.blogs = {'Test': blog}
+        with patch('app.print_post') as mocked_print_post:
+            app.print_posts(blog)
+            mocked_print_post.assert_called_with(blog.posts[0])
+
+    def test_print_post(self):
+        post = Post('Test Post', 'Test Content')
+        expected_print = "Title: Test Post, content: Test Content"
+        with patch('builtins.print') as mocked_print:
+            app.print_post(post)
+            mocked_print.assert_called_with(expected_print)
